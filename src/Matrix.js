1d34880369eb6573f587c31de3192fc7167416ad
Wesley <wes.beck21@outlook.com>
2021-03-17 16:23:15 -0700

    commit tree 08192e463b4d8d8b33e14d8f46029451a7d04e1e

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Cell from './Cell.js'
 
 export default class Matrix extends Component {
   
+    
+
+
   genRow = (vals) => {
-    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    return vals.map(val => <Cell value={val}/>) // replace me and render a cell component instead!
   }
   
   genMatrix = () => {
@@ -17,5 +21,15 @@ export default class Matrix extends Component {
       </div>
     )
   }
-  
+
+
+}
+
+Matrix.defaultProps = {
+  values: (() => {
+    const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+    return (new Array(10).fill(defRow))
+  })()
+    
+    
 }
--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import './index.css';
 import { pattern1 } from './data.js'
 import Matrix from './Matrix.js'
 
+
 ReactDOM.render(<Matrix values={pattern1} />, document.getElementById('root'));
